18bc698f8fef23d2f2125955b384053949e6542d
LiYong <2236472089@qq.com>
2023-08-29 23:33:42 +0800

    refactor: 抽规则配置

--- packages-config/stylelintrc/react.js
+++ packages-config/stylelintrc/react.js
@@ -1,22 +1,16 @@
 const propertiesOrder = require("./properties-order");
+const common = require("./common");
+
 module.exports = {
     "processors": [
         "stylelint-processor-styled-components"
-    ],
-    "plugins": [
-        "stylelint-order" // css属性的先后顺序
     ],
     "extends": [
         "stylelint-config-standard",
         "stylelint-config-styled-components",
     ],
     "rules": {
-        // 禁止空块
-        'block-no-empty': true,
-        // 禁止低优先级的选择器出现在高优先级的选择器之后。
-        "no-descending-specificity": null,
-        // 禁止空注释
-        "comment-no-empty": true,
+        ...common.rules,
         'order/properties-order': [[...propertiesOrder], {
             unspecified: 'ignore' // 让 styled-components 的 mixin 可以按需要放置
         }]
--- packages-config/stylelintrc/vue.js
+++ packages-config/stylelintrc/vue.js
@@ -1,4 +1,5 @@
 const propertiesOrder = require("./properties-order");
+const common = require("./common");
 
 module.exports = {
     "plugins": [
@@ -8,13 +9,9 @@ module.exports = {
         "stylelint-config-recommended-vue"
     ],
     "rules": {
-        // 禁止空块
-        'block-no-empty': true,
-        // 禁止低优先级的选择器出现在高优先级的选择器之后。
-        "no-descending-specificity": null,
-        // 禁止空注释
-        "comment-no-empty": true,
+        ...common.rules,
         // css 排序
         'order/properties-order': propertiesOrder
+
     }
 }
