a34a1e87df3626c1ac6d2fa63f621bc8c59d744b
LiYong <2236472089@qq.com>
2023-08-29 23:40:31 +0800

    feat: 格式化

--- packages-config/index.js
+++ packages-config/index.js
@@ -1,1 +1,13 @@
 alert('请阅读文档，按需引用。');
+
+
+/**
+ * stylelintrc 参考案例：
+ *
+ * https://juejin.cn/post/6940127032932040735
+ * https://github.com/aliyun/alibabacloud-console-base/tree/master/packages-dev/stylelint-config
+ * https://www.npmjs.com/package/@alicloud/stylelint-config
+ *
+ *
+ *
+ */
