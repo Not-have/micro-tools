6373d154943a9cb39415195b68370014e076fda3
李勇 <18149302282@163.com>
2023-11-14 10:45:02 +0800

    feat: add npm-package-json-lint

--- packages-stylelint-conf/react.js
+++ packages-stylelint-conf/react.js
@@ -1,17 +1,17 @@
-const propertiesOrder = require("./properties-order");
+const propertiesOrder = require('./properties-order');
 
 module.exports = {
-    "processors": [
-        "stylelint-processor-styled-components"
+    'processors': [
+        'stylelint-processor-styled-components'
     ],
-    "extends": [
-        "./common",
-        "stylelint-config-standard",
-        "stylelint-config-styled-components",
+    'extends': [
+        './common',
+        'stylelint-config-standard',
+        'stylelint-config-styled-components'
     ],
-    "rules": {
+    'rules': {
         'order/properties-order': [[...propertiesOrder], {
             unspecified: 'ignore' // 让 styled-components 的 mixin 可以按需要放置
         }]
     }
-}
+};
