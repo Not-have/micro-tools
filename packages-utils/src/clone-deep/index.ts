ed8d262302003a9ed6e10e31a5189b41daae6dc4
苟且偷安 <2236472089@qq.com>
2024-02-20 01:21:50 +0800

    fix: cloneDeep 容错处理

--- packages-utils/src/clone-deep/index.ts
+++ packages-utils/src/clone-deep/index.ts
@@ -16,78 +16,82 @@ import isFunction from '../is-function';
  * ② 无法循环引用
  */
 export default function cloneDeep<T extends object>(value: T | symbol, map = new WeakMap()): T | {} {
-    // Set 类型
-    if (value instanceof Set) {
+    try {
+        // Set 类型
+        if (value instanceof Set) {
+            if (map.has(value)) {
+                return map.get(value);
+            }
+
+            const newSet = new Set();
+            map.set(value, newSet);
+
+            value.forEach((item) => {
+                newSet.add(cloneDeep(item, map));
+            });
+
+            return newSet;
+        }
+
+        // Map 类型
+        if (value instanceof Map) {
+            if (map.has(value)) {
+                return map.get(value);
+            }
+
+            const newMap = new Map();
+            map.set(value, newMap);
+
+            value.forEach((val, key) => {
+                newMap.set(cloneDeep(key, map), cloneDeep(val, map));
+            });
+
+            return newMap;
+        }
+
+        // Symbol 类型
+        if (typeof value === 'symbol') {
+            return Symbol(value.description);
+        }
+
+        // 函数类型
+        if (isFunction(value)) {
+            return function(...args: any[]) {
+                return value.apply(this, args);
+            };
+        }
+
+        // 对象类型
+        if (!isObject(value)) {
+            return value;
+        }
+
+        // 判断 map 中是否有值，有了直接 return
         if (map.has(value)) {
             return map.get(value);
         }
 
-        const newSet = new Set();
-        map.set(value, newSet);
+        // 判断传入的对象是数组, 还是对象
+        const newValue = Array.isArray(value) ? [] : {};
 
-        value.forEach((item) => {
-            newSet.add(cloneDeep(item, map));
-        });
+        // map 没有值
+        map.set(value, newValue);
 
-        return newSet;
-    }
-
-    // Map 类型
-    if (value instanceof Map) {
-        if (map.has(value)) {
-            return map.get(value);
+        for (const key in value) {
+            // @ts-ignore
+            newValue[key] = cloneDeep(value[key], map);
         }
 
-        const newMap = new Map();
-        map.set(value, newMap);
+        const symbolKeys = Object.getOwnPropertySymbols(value);
 
-        value.forEach((val, key) => {
-            newMap.set(cloneDeep(key, map), cloneDeep(val, map));
-        });
+        for (const sKey of symbolKeys) {
+            // Symbol 作为 key 的处理（这个可以不做 Symbol(sKey.description) 处理）
+            const newSKey = Symbol(sKey.description);
+            newValue[newSKey] = cloneDeep(value[sKey], map);
+        }
 
-        return newMap;
+        return newValue;
+    }catch {
+        throw new Error('Deep clone handle error!');
     }
-
-    // Symbol 类型
-    if (typeof value === 'symbol') {
-        return Symbol(value.description);
-    }
-
-    // 函数类型
-    if (isFunction(value)) {
-        return function(...args: any[]) {
-            return value.apply(this, args);
-        };
-    }
-
-    // 对象类型
-    if (!isObject(value)) {
-        return value;
-    }
-
-    // 判断 map 中是否有值，有了直接 return
-    if (map.has(value)) {
-        return map.get(value);
-    }
-
-    // 判断传入的对象是数组, 还是对象
-    const newValue = Array.isArray(value) ? [] : {};
-
-    // map 没有值
-    map.set(value, newValue);
-
-    for (const key in value) {
-        // @ts-ignore
-        newValue[key] = cloneDeep(value[key], map);
-    }
-
-    const symbolKeys = Object.getOwnPropertySymbols(value);
-
-    for (const sKey of symbolKeys) {
-        // Symbol 作为 key 的处理（这个可以不做 Symbol(sKey.description) 处理）
-        const newSKey = Symbol(sKey.description);
-        newValue[newSKey] = cloneDeep(value[sKey], map);
-    }
-
-    return newValue;
 }
