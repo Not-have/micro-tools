3cad788aaf339dd997ba2243e764453f3b624bd2
苟且偷安 <2236472089@qq.com>
2024-02-20 21:41:42 +0800

    feat: 新增 isUndefined

--- packages-utils/src/is-function/index.ts
+++ packages-utils/src/is-function/index.ts
@@ -1,5 +1,7 @@
 /**
  * 是否函数类型判断
+ * @param value
+ * @return {value is Function} 如果 value 为 function，则返回 true，否则返回 false
  */
 export default function isFunction(value: any): value is Function {
     return typeof value === 'function';
--- packages-utils/src/is-object/index.ts
+++ packages-utils/src/is-object/index.ts
@@ -3,6 +3,8 @@
  *
  * value is Record<string, any>
  * 函数的返回类型是 value is Record<string, any>，这种形式的返回类型是 TypeScript 中用于表达类型守卫的一种方式。这意味着如果函数返回 true，TypeScript 将会把传入的参数视为 Record<string, any> 类型；如果返回 false，则参数不会被视为这个类型。
+ *
+ * @return 如果 value 为 object，则返回 true，否则返回 false
  */
 export default function isObject(value: unknown): value is Record<string, any> {
     const type: 'string' | 'number' | 'bigint' | 'boolean' | 'symbol' | 'undefined' | 'object' | 'function' | 'null' = typeof value;
