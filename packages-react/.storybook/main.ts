990e4c60283afaab7fa1d41f65a3e331bdf41435
苟且偷安 <2236472089@qq.com>
2024-02-28 01:53:43 +0800

    feat: packages-react

--- packages-react/.storybook/main.ts
+++ packages-react/.storybook/main.ts
@@ -3,13 +3,13 @@ import type { StorybookConfig } from "@storybook/react-webpack5";
 const config: StorybookConfig = {
     stories: [
         "../stories/**/*.mdx",
-        "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+        "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
     ],
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-onboarding",
-        "@storybook/addon-interactions",
+        "@storybook/addon-interactions"
     ],
     framework: {
         name: "@storybook/react-webpack5",
@@ -20,7 +20,7 @@ const config: StorybookConfig = {
         },
     },
     docs: {
-        autodocs: "tag",
-    },
+        autodocs: "tag"
+    }
 };
 export default config;
