4adcdf17f5c9d38663b999042642bb06e0386d34
苟且偷安 <2236472089@qq.com>
2024-02-27 23:55:50 +0800

    refactor: 更改文件名

---
+++ packages-react/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="App">
+            2222
+        </div>
+    );
+}
+
+export default App;
--- packages-react/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <>
-            1111
-        </>
-    );
-}
