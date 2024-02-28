c74fa43fc81465f43cebb783a540d4356114263a
苟且偷安 <2236472089@qq.com>
2024-02-29 01:07:19 +0800

    feat: update micro-util-ts

--- packages-utils/src/map-gaode/index.ts
+++ packages-utils/src/map-gaode/index.ts
@@ -9,6 +9,35 @@ import {
  * 创建高德地图
  * @param param0
  * @returns 返回高德地图的实例
+ *
+ * 使用：
+ *
+ * mapGaode({
+ *     key: 'Xxx',
+ *     securityCode: 'Xxx',
+ *     plugins: ['AMap.Walking']
+ * }).then((AMap: any) => {
+ *     var map = new AMap.Map('container', {
+ *         resizeEnable: true,
+ *         center: [116.397428, 39.90923], //地图中心点
+ *         zoom: 13 //地图显示的缩放级别
+ *     });
+ *
+ *     //步行导航
+ *     var walking = new AMap.Walking({
+ *         map: map,
+ *         panel: 'panel'
+ *     });
+ *     //根据起终点坐标规划步行路线
+ *     walking.search([116.399028, 39.845042], [116.436281, 39.880719], function (status, result) {
+ *         // result即是对应的步行路线数据信息，相关数据结构文档请参考  https://lbs.amap.com/api/javascript-api/reference/route-search#m_WalkingResult
+ *         if (status === 'complete') {
+ *             console.log('绘制步行路线完成');
+ *         } else {
+ *             console.error('步行路线数据查询失败' + result);
+ *         }
+ *     });
+ * });
  */
 export default async function mapGaode({
     key,
--- packages-utils/src/map-gaode/types/index.ts
+++ packages-utils/src/map-gaode/types/index.ts
@@ -1,6 +1,18 @@
 export interface IParams {
+    /**
+     * 申请好的 Web 端开发者 Key，调用 load 时必填
+     */
     key: string,
+    /**
+     * 安全密钥
+     */
     securityCode?: string,
+    /**
+     * 指定要加载的 JS API 的版本，缺省时默认为 2.0
+     */
     version?: string,
+    /**
+     * 需要使用的的插件列表
+     */
     plugins?: Array<string>
 }
