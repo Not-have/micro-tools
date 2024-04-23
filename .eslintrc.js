6516eac3c9757417f66038c47d09263987321a2c
苟且偷安 <2236472089@qq.com>
2024-04-23 23:17:20 +0800

    refactor: 迁移

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
-module.exports = {
-    extends: ['micro-eslint-conf/vue'].map(path => require.resolve(path)),
-    'rules': {
-        'css-vars/no-undef-vars': 0
-    }
-};
+// module.exports = {
+//     extends: ['micro-eslint-conf/vue'].map(path => require.resolve(path)),
+//     'rules': {
+//         'css-vars/no-undef-vars': 0
+//     }
+// };
