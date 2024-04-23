fb7e421bd238df7ae60228ad3d1901b68b66eb26
苟且偷安 <2236472089@qq.com>
2024-04-23 23:48:11 +0800

    refactor: module re-division

---
+++ packages-eslint-conf/build.config.ts
@@ -0,0 +1,14 @@
+/**
+ * docs:
+ * https://zhuanlan.zhihu.com/p/671414552
+ */
+// import { defineBuildConfig } from 'unbuild';
+
+// export default defineBuildConfig({
+//     clean: true,
+//     entries: ['src/ts', 'src/vue'],
+//     declaration: true,
+//     rollup: {
+//         emitCJS: true,
+//     },
+// });
