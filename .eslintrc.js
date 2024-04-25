a4fd209af7b6c5eec0bf6aed2ab561bb2f75b9aa
苟且偷安 <2236472089@qq.com>
2024-04-25 22:39:57 +0800

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,9 @@
-// module.exports = {
-//     extends: ['micro-eslint-conf/vue'].map(path => require.resolve(path)),
-//     'rules': {
-//         'css-vars/no-undef-vars': 0
-//     }
-// };
-
 module.exports = {
-    parserOptions: {
-        sourceType: 'script',
-    },
+    extends: ['micro-eslint-conf/index'].map(require.resolve)
 };
+
+// module.exports = {
+//     parserOptions: {
+//         sourceType: 'script',
+//     },
+// };
