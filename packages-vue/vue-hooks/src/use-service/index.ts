f06f64eabe6e91029a866d6a1269815c2fbbf449
苟且偷安 <2236472089@qq.com>
2024-05-03 05:51:08 +0800

    feat: useService

---
+++ packages-vue/vue-hooks/src/use-service/index.ts
@@ -0,0 +1,114 @@
+import {
+  UnwrapRef,
+  toRefs,
+  reactive,
+  watch,
+  isReactive,
+  isRef
+} from "vue";
+
+import {
+  debounce as _debounce
+} from "micro-util-ts";
+
+import {
+  IServiceFunction,
+  IStateResult,
+  IConfig,
+  IAsyncResult
+} from "./types";
+
+/**
+ *
+ * @param {IServiceFunction<T, Q>} fetch
+ * @param {Q} query
+ * @param {T} initData
+ * @param {IConfig} config
+ * @return {IAsyncResult} 返回 {
+ *     run, // 返回一个执行的方法
+ *     data, // 请求的数据
+ *     loading // 当前的请求状态
+ * }
+ */
+export default function useService<T, Q>(fetch: IServiceFunction<T, Q>, query?: Q, initData?: T, config: IConfig = {
+  debounce: false,
+  error: undefined,
+  immediate: true,
+  watchQuery: false
+}): IAsyncResult<T, Q> {
+  const stateResult = reactive<IStateResult<T>>({
+    data: initData || null,
+    error: null,
+    loading: false
+  });
+
+  const {
+    immediate,
+    debounce,
+    watchQuery,
+    error
+  } = config;
+
+  const asyncFunction = (arg?: Q): Promise<T> => {
+    stateResult.loading = true;
+
+    return new Promise((reactive, reject) => {
+      fetch(arg).then((res: T) => {
+        stateResult.loading = false;
+        stateResult.data = res as UnwrapRef<T> | null;
+        reactive(res);
+      }).
+        catch(err => {
+          stateResult.loading = false;
+          stateResult.error = err;
+
+          if (error) {
+            error(err);
+          }
+
+          reject(err);
+        });
+    });
+  };
+
+  const debounceFn = _debounce(asyncFunction, typeof (debounce) === "number" ? debounce : 300, true);
+
+  const run = (arg?: Q): Promise<T> => {
+    if (debounce !== false) {
+      return debounceFn(arg) as Promise<T>;
+    }
+
+    return asyncFunction(arg);
+  };
+
+  if (immediate) {
+    run(query);
+  }
+
+  // TODO 优化
+  if (watchQuery && (isReactive(query) || isRef(query))) {
+
+    // @ts-ignore
+    watch(query, (newQuery: Q) => {
+      run(newQuery);
+    }, {
+      deep: true, // 深度监听
+      immediate: false // 立即执行（西药第一次 进来就打印）
+    });
+  } else {
+    throw new Error("Query is not reactive,unable to proceed watch.");
+  }
+
+  const {
+    data,
+    loading,
+    error: err
+  } = toRefs(stateResult);
+
+  return {
+    run,
+    data,
+    loading,
+    error: err
+  };
+}
---
+++ packages-vue/vue-hooks/src/use-service/types/index.ts
@@ -0,0 +1,75 @@
+import type {
+  Ref,
+  UnwrapRef
+} from "vue";
+
+export interface IServiceFunction<T, Q> {
+  (arg?: Q): Promise<T>;
+}
+
+export interface IStateResult<T> {
+  data: null | T,
+  loading: boolean,
+  error: PromiseRejectionEvent | null
+}
+
+export interface IConfig {
+
+  /**
+   * 是否直接执行
+   */
+  immediate?: boolean,
+
+  /**
+   * 防抖
+   */
+  debounce?: boolean | number,
+
+  /**
+   * 是否监听 query 参数的改变（只能监听 new Proxy）
+   * 当 query 改变时，去请求数据
+   */
+  watchQuery?: boolean,
+
+  /**
+   * 请求错误时的展示
+   * 也是请求失败时的处理
+   */
+  error?: Function
+}
+
+export interface IAsyncResult<T, Q> {
+  run: (arg?: Q) => Promise<T>;
+  data: Ref<UnwrapRef<T> | null>;
+  loading: Ref<boolean>;
+
+  /**
+   * @throws 接口请求错误
+   *
+   * 他应该在 main.ts 中进行全局的处理
+   *
+   * function registerPromiseErrorHandler() {
+   *     window.addEventListener('unhandledrejection', function (event) {
+   *         console.log(event);
+   *         // 处理错误
+   *         ...
+   *     }, true);
+   * }
+   *
+   * main.ts 中调用
+   *
+   * 推荐 配置全局错误处理时创建一个 utils/error-handle 文件来进行管理
+   *
+   * registerPromiseErrorHandler(); // 建议放在 errorHandler 之后
+   *
+   * 模拟:
+   *
+   * function handleError() { // 点击时触发
+   *     new Promise((resolve, reject) => {
+   *         reject('1111');
+   *     }).catch
+   * }
+   *
+   */
+  error: Ref<PromiseRejectionEvent | null>
+}
