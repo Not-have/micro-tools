becbe3abec57fdda315a114ff909ff7cbc40e2aa
苟且偷安 <2236472089@qq.com>
2024-05-09 23:27:28 +0800

    feat: micro-element-plus-extra

--- scripts/check-install.js
+++ scripts/check-install.js
@@ -1,25 +1,36 @@
-// scripts/check-install.js
+// Scripts/check-install.js
 
 /**
  * @deprecated 运行下来出 👾 了
  */
-const execSync = require('child_process').execSync;
+const {
+  execSync
+} = require("child_process");
+
 try {
-    // 尝试运行 only-allow 和 husky install 命令
-    execSync('npx only-allow pnpm');
-    console.log('Running "only-allow" command...');
-    /**
+
+  // 尝试运行 only-allow 和 husky install 命令
+  execSync("npx only-allow pnpm");
+  // eslint-disable-next-line no-console
+  console.log("Running \"only-allow\" command...");
+
+  /**
      * 从 package.json 中 抽离到这的，原内容    "prepare": "husky install",
      */
-    execSync('husky install');
-    console.log('Running "husky install" command...');
+  execSync("husky install");
+  // eslint-disable-next-line no-console
+  console.log("Running \"husky install\" command...");
 
-    console.log(process.argv);
-    // 如果上述命令没有报错，继续安装依赖
-    execSync('pnpm run boot');
-    console.log('Running "pnpm install" command...');
+  // eslint-disable-next-line no-console
+  console.log(process.argv);
+
+  // 如果上述命令没有报错，继续安装依赖
+  execSync("pnpm run boot");
+  // eslint-disable-next-line no-console
+  console.log("Running \"pnpm install\" command...");
 } catch (error) {
-    execSync('pnpm run clear && pnpm run clear:lock');
-    console.error('error:', error.message + '，请使用 pnpm 下载依赖');
-    process.exit(1); // 终止进程
+  execSync("pnpm run clear && pnpm run clear:lock");
+  // eslint-disable-next-line no-console
+  console.error("error:", `${error.message }，请使用 pnpm 下载依赖`);
+  process.exit(1); // 终止进程
 }
--- scripts/merge-markdown.js
+++ scripts/merge-markdown.js
@@ -3,58 +3,78 @@
  * 第二个参数 合并后的文档地址
  * "test": "node ./scripts/merge-markdown.js"
  */
-
-const fs = require('fs');
-const path = require('path');
+import {
+  readdirSync,
+  readFileSync,
+  statSync,
+  writeFileSync
+} from "fs";
+// eslint-disable-next-line sort-imports
+import {
+  extname,
+  join
+} from "path";
 
 try {
-    // 定义一个函数来获取指定文件夹下的所有 Markdown 文件路径
-    const getAllMarkdownFiles = folderPath => {
-        const files = fs.readdirSync(folderPath);
 
-        const markdownFiles = files.filter(file => {
-            const filePath = path.join(folderPath, file);
-            const fileStats = fs.statSync(filePath);
-            return fileStats.isFile() && path.extname(filePath).toLowerCase() === '.md';
-        });
+  // 定义一个函数来获取指定文件夹下的所有 Markdown 文件路径
+  const getAllMarkdownFiles = folderPath => {
+    const files = readdirSync(folderPath);
 
-        return markdownFiles.map(file => path.join(folderPath, file));
-    };
+    const markdownFiles = files.filter(file => {
+      const filePath = join(folderPath, file);
 
-    // 定义一个函数来读取并合并多个 Markdown 文件的内容
-    const mergeMarkdownFiles = (filePaths, outputFile, title) => {
-        console.log(filePaths, outputFile, title);
-        try {
-            // 在文件之间添加一级标题和空行来分隔
-            const mergedContent = title ? `# ${ title }\n\n` : '' + filePaths
-                .map(filePath => fs.readFileSync(filePath, 'utf-8'))
-                .join('\n' + '---' + '\n\n'); // 在文件之间添加空行来分隔
+      const fileStats = statSync(filePath);
 
-            fs.writeFileSync(outputFile, mergedContent);
-            // console.log('Markdown 文件合并完成！');
-        } catch (error) {
-            // console.error('合并过程出错：', error);
-        }
-    };
+      return fileStats.isFile() && extname(filePath).toLowerCase() === ".md";
+    });
 
-    const paths = process.argv.slice(2);
+    return markdownFiles.map(file => join(folderPath, file));
+  };
 
-    if (paths.length !== 2) {
-        console.error('❌ Markdown Url Error');
-        process.exit(1); // 终止进程
+  // 定义一个函数来读取并合并多个 Markdown 文件的内容
+  const mergeMarkdownFiles = (filePaths, outputFile, title) => {
+    // eslint-disable-next-line no-console
+    console.log(filePaths, outputFile, title);
+
+    try {
+
+      // 在文件之间添加一级标题和空行来分隔
+      const mergedContent = title ? `# ${ title }\n\n` : `${ filePaths.
+          map(filePath => readFileSync(filePath, "utf-8")).
+          // eslint-disable-next-line no-useless-concat
+          join("\n" + "---" + "\n\n")}`; // 在文件之间添加空行来分隔
+
+      writeFileSync(outputFile, mergedContent);
+
+      // Console.log('Markdown 文件合并完成！');
+    } catch (error) {
+
+      // Console.error('合并过程出错：', error);
     }
+  };
 
-    // 传入要合并的 Markdown 文件路径数组和输出文件的路径
-    const targetFolderPath = paths[0]; // 替换为实际的文件夹路径
+  const paths = process.argv.slice(2);
 
-    // 获取所有 Markdown 文件路径
-    const markdownFilePaths = getAllMarkdownFiles(targetFolderPath);
+  if (paths.length !== 2) {
+    // eslint-disable-next-line no-console
+    console.error("❌ Markdown Url Error");
+    process.exit(1); // 终止进程
+  }
 
-    const outputFilePath = paths[1] + '/README.md'; // 替换为输出文件的路径
+  // 传入要合并的 Markdown 文件路径数组和输出文件的路径
+  const targetFolderPath = paths[0]; // 替换为实际的文件夹路径
 
-    mergeMarkdownFiles(markdownFilePaths, outputFilePath);
+  // 获取所有 Markdown 文件路径
+  const markdownFilePaths = getAllMarkdownFiles(targetFolderPath);
 
-    console.log('✅ Markdown Merge Success');
+  const outputFilePath = `${paths[1] }/README.md`; // 替换为输出文件的路径
+
+  mergeMarkdownFiles(markdownFilePaths, outputFilePath);
+
+  // eslint-disable-next-line no-console
+  console.log("✅ Markdown Merge Success");
 } catch {
-    console.error('❌ Markdown Merge Fail');
+  // eslint-disable-next-line no-console
+  console.error("❌ Markdown Merge Fail");
 }
