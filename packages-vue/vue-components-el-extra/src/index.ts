21374579ffe065b9a19756f4d079e0f6c86c8cd8
苟且偷安 <2236472089@qq.com>
2024-06-08 01:52:10 +0800

    fix: eslint

--- packages-vue/vue-components-el-extra/src/index.ts
+++ packages-vue/vue-components-el-extra/src/index.ts
@@ -3,9 +3,10 @@ import "./style";
 
 export * from "./button";
 export * from "./input";
+
 export { default as opDialog } from "./op-dialog";
 
-// Form
+// form
 export {
   Form
 } from "./form";
