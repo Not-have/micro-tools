052f761d60050991ba30698bc8469794805c61e2
苟且偷安 <2236472089@qq.com>
2024-06-08 02:11:51 +0800

    feat: micro-stylelint-conf

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -8,6 +8,10 @@ module.exports = {
       files: ["packages-vue/**/*.{js,vue,ts,tsx}"],
       customSyntax: 'postcss-html',
       extends: ["micro-stylelint-conf/vue"].map(require.resolve)
+    },
+    {
+      files: ["packages-react/**/*.{jsx,ts,tsx}"],
+      extends: ["micro-stylelint-conf/react"]
     }
   ]
 };
