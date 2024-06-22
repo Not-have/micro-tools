4417971d1d696782d38fb9c1a4b94beba8135dba
苟且偷安 <2236472089@qq.com>
2024-06-22 23:36:55 +0800

    feat: 删除无用 demo

--- packages-vue/vue-components-el-extra/stories/demo-dialog/content/index.vue
+++ packages-vue/vue-components-el-extra/stories/demo-dialog/content/index.vue
@@ -1,14 +1,10 @@
 <script lang="ts" setup>
 import {
-  reactive,
   onMounted,
   ref,
   unref
 } from "vue";
 import {
-  ElForm,
-  ElFormItem,
-  ElInput,
   ElCard
 } from "element-plus";
 
@@ -21,10 +17,6 @@ const {
   setValues,
   setValue
 } = useFields();
-
-const numberValidateForm = reactive({
-  age: ""
-});
 
 const elFormRef = ref();
 
@@ -42,39 +34,16 @@ const handleClick = (): void => {
 </script>
 
 <template>
-  <ElForm
-    ref="elFormRef"
-    style="max-width: 600px"
-    :model="numberValidateForm"
-    label-width="auto"
-    class="demo-ruleForm"
+  <ElCard
+    style="max-width: 480px"
+    @click="handleClick"
   >
-    <ElFormItem
-      label="age"
-      prop="age"
-      :rules="[
-        { required: true, message: 'age is required' },
-        { type: 'number', message: 'age must be a number' },
-      ]"
-    >
-      <ElInput
-        v-model.number="numberValidateForm.age"
-        type="text"
-        autocomplete="off"
-      />
-    </ElFormItem>
-
-    <ElCard
-      style="max-width: 480px"
-      @click="handleClick"
-    >
-      <template #header>
-        修改值
-      </template>
-      <img
-        src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-        style="width: 100%"
-      />
-    </ElCard>
-  </ElForm>
+    <template #header>
+      修改值
+    </template>
+    <img
+      src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+      style="width: 100%"
+    />
+  </ElCard>
 </template>
