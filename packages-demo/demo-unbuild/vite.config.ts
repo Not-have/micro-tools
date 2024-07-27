7fe5e0cf0a4c5be1035c768d4f72e47558f38b00
苟且偷安 <2236472089@qq.com>
2024-07-27 17:07:39 +0800

    feat: demo

--- packages-demo/demo-unbuild/vite.config.ts
+++ packages-demo/demo-unbuild/vite.config.ts
@@ -3,7 +3,7 @@ import {
 } from "vite";
 
 export default defineConfig({
-  root: "public",  // 设置根目录为 public
+  root: "stories",  // 设置根目录为 public
   build: {
     rollupOptions: {
       input: "/stories/index.html"  // 设置入口 HTML 文件的位置
---
+++ packages-demo/demo/src/main.ts
@@ -0,0 +1,5 @@
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    demo
+  </div>
+`;
