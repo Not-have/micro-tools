a2357618994421fd87bb308182d8b4111f4e871c
李勇 <2236472089@qq.com>
2025-03-29 23:05:22 +0800

    feat: @mt-kit/prettier-config 配置完成

---
+++ packages-dev/prettier-config/README.md
@@ -0,0 +1,45 @@
+# @mt-kit/prettier-config
+
+[docs](https://prettier.nodejs.cn/)
+
+## 安装
+
+```bash
+npm i prettier @mt-kit/prettier-config -D
+```
+
+## 使用
+
+新建 `prettier.config.mjs` 文件
+
+```js
+export { default } from "@mb-kit/prettier-config";
+```
+
+注：推荐配合 [@mt-kit/eslint-config](https://www.npmjs.com/package/@mt-kit/eslint-config) 一块使用。
+
+## 忽略文件
+
+新建 `.prettierignore` 文件
+
+```.prettierignore
+dist
+*-dist
+.local
+.output.js
+node_modules
+.nvmrc
+coverage
+CODEOWNERS
+.nitro
+.output
+
+
+**/*.svg
+**/*.sh
+
+public
+.npmrc
+**/*-lock.yaml
+**/*-lock.json
+```
---
+++ packages-dev/prettier-config/index.mjs
@@ -0,0 +1,45 @@
+/**
+ * https://prettier.nodejs.cn/docs/en/configuration.html
+ */
+export default {
+  // 设置代码行结束符
+  endOfLine: "auto",
+
+  // 针对特定文件类型的重写配置
+  overrides: [
+    {
+      // 匹配所有 `.json5` 扩展名的文件
+      files: ["*.json5"],
+
+      // 为 `.json5` 文件配置的格式化选项
+      options: {
+        // 保留属性名称的引号样式 (保留原始样式，不进行更改)
+        quoteProps: "preserve",
+
+        // 使用双引号 (单引号被禁用)
+        singleQuote: false
+      }
+    }
+  ],
+
+  // 设置代码的最大行宽度
+  printWidth: 80,
+
+  // 禁止自动换行，代码始终保持一行
+  proseWrap: "never",
+
+  // 是否在语句末尾添加分号
+  semi: true,
+
+  // 使用单引号（除非是字符串内部有单引号时）
+  // singleQuote: true,
+
+  // 对象和数组的尾部是否添加逗号
+  trailingComma: "none",
+
+  // 在箭头函数只有一个参数时，不保留圆括号
+  arrowParens: "avoid",
+
+  // 在 HTML、Vue 和 JSX 中，将属性放在单独的行上
+  singleAttributePerLine: true
+};
