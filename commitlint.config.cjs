3c5a8177d4075f3554b15101330e656eb15b2734
李勇 <2236472089@qq.com>
2025-03-29 20:13:58 +0800

    test: 测试

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -48,13 +48,7 @@ module.exports = {
     ]
   },
   "prompt": {
-    "alias": {
-      b: "build: bump dependencies",
-      c: "chore: update config",
-      f: "docs: fix typos",
-      r: "docs: update README",
-      s: "style: update code format"
-    },
+    "alias": {},
     "messages": {
       "type": "选择你要提交的类型 :",
       "scope": "选择一个提交范围（可选）:",
@@ -68,20 +62,20 @@ module.exports = {
       "confirmCommit": "是否提交或修改commit ?"
     },
     "types": [
-      { value: 'feat', name: 'feat:     ✨ 新功能' },
-      { value: 'fix', name: 'fix:      🐛 修复Bug' },
-      { value: 'docs', name: 'docs:     📝 文档相关' },
-      { value: 'style', name: 'style:    💄 代码格式（空格、分号等）' },
-      { value: 'refactor', name: 'refactor: ♻️ 代码重构（非功能不影响）' },
-      { value: 'perf', name: 'perf:     ⚡ 性能优化' },
-      { value: 'test', name: 'test:     ✅ 测试相关' },
-      { value: 'build', name: 'build:    🏗️ 构建相关' },
-      { value: 'ci', name: 'ci:       ⚙️ CI配置' },
-      { value: 'chore', name: 'chore:    🔨 其他修改' },
-      { value: 'revert', name: 'revert:   ⏪ 回滚' },
-      { value: 'release', name: 'release:  🚀 发布版本' },
-      { value: 'types', name: 'types:    🟣 类型相关（TypeScript）' },
-      { value: 'workflow', name: 'workflow: 🔄 工作流相关' }
+      { "value": "feat", "name": "feat:     ✨ 新功能" },
+      { "value": "fix", "name": "fix:      🐛 修复Bug" },
+      { "value": "docs", "name": "docs:     📝 文档相关" },
+      { "value": "style", "name": "style:    💄 代码格式（空格、分号等）" },
+      { "value": "refactor", "name": "refactor: ♻️ 代码重构（非功能不影响）" },
+      { "value": "perf", "name": "perf:     ⚡ 性能优化" },
+      { "value": "test", "name": "test:     ✅ 测试相关" },
+      { "value": "build", "name": "build:    🏗️ 构建相关" },
+      { "value": "ci", "name": "ci:       ⚙️ CI配置" },
+      { "value": "chore", "name": "chore:    🔨 其他修改" },
+      { "value": "revert", "name": "revert:   ⏪ 回滚" },
+      { "value": "release", "name": "release:  🚀 发布版本" },
+      { "value": "types", "name": "types:    🟣 类型相关（TypeScript）" },
+      { "value": "workflow", "name": "workflow: 🔄 工作流相关" }
     ],
     "emoji": true // 开启 emoji
   }
