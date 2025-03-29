deba4ded1df39966af7caf0051440c35d5aee64d
李勇 <2236472089@qq.com>
2025-03-29 20:13:10 +0800

    refactor: 重新构建

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,4 @@
+// npx git-cz  也可提交
 module.exports = {
   // 继承 commitlint 的默认规则，遵循 conventional commits 规范
   extends: ["@commitlint/config-conventional"],
@@ -45,5 +46,43 @@ module.exports = {
         "release"   // 🚀 发布版本
       ]
     ]
+  },
+  "prompt": {
+    "alias": {
+      b: "build: bump dependencies",
+      c: "chore: update config",
+      f: "docs: fix typos",
+      r: "docs: update README",
+      s: "style: update code format"
+    },
+    "messages": {
+      "type": "选择你要提交的类型 :",
+      "scope": "选择一个提交范围（可选）:",
+      "customScope": "请输入自定义的提交范围 :",
+      "subject": "填写简短精炼的变更描述 :\n",
+      "body": "填写更加详细的变更描述（可选）。使用 \"|\" 换行 :\n",
+      "breaking": "列举非兼容性重大的变更（可选）。使用 \"|\" 换行 :\n",
+      "footerPrefixesSelect": "选择关联issue前缀（可选）:",
+      "customFooterPrefix": "输入自定义issue前缀 :",
+      "footer": "列举关联issue (可选) 例如: #31, #I3244 :\n",
+      "confirmCommit": "是否提交或修改commit ?"
+    },
+    "types": [
+      { value: 'feat', name: 'feat:     ✨ 新功能' },
+      { value: 'fix', name: 'fix:      🐛 修复Bug' },
+      { value: 'docs', name: 'docs:     📝 文档相关' },
+      { value: 'style', name: 'style:    💄 代码格式（空格、分号等）' },
+      { value: 'refactor', name: 'refactor: ♻️ 代码重构（非功能不影响）' },
+      { value: 'perf', name: 'perf:     ⚡ 性能优化' },
+      { value: 'test', name: 'test:     ✅ 测试相关' },
+      { value: 'build', name: 'build:    🏗️ 构建相关' },
+      { value: 'ci', name: 'ci:       ⚙️ CI配置' },
+      { value: 'chore', name: 'chore:    🔨 其他修改' },
+      { value: 'revert', name: 'revert:   ⏪ 回滚' },
+      { value: 'release', name: 'release:  🚀 发布版本' },
+      { value: 'types', name: 'types:    🟣 类型相关（TypeScript）' },
+      { value: 'workflow', name: 'workflow: 🔄 工作流相关' }
+    ],
+    "emoji": true // 开启 emoji
   }
 }
