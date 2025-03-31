32e88907a3c0074c6f4a4250f26bc7fdddbf4f7f
李勇 <2236472089@qq.com>
2025-03-31 23:30:03 +0800

    test: 格式化测试-02

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,16 @@
 export default {
   extends: ["@mt-kit/stylelint-config"],
+  ignoreFiles: [
+    "**/*.json",
+    "**/*.mjs",
+    "**/*.cjs",
+    "**/*.svg",
+    "**/*.ico",
+    "**/*.js",
+    "**/*.ts",
+    "node_modules/**",
+    "dist/**",
+    "**/*.md"
+  ],
   root: true
 };
