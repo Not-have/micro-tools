f545ecd45215592d97ae64c69efa292a8a6657cb
李勇 <2236472089@qq.com>
2025-04-02 00:36:44 +0800

    feat: 添加 debounce 和 throttle 函数，重命名 handle-object 为 object-value-to-string 并导出默认函数

--- packages-utils/src/index.ts
+++ packages-utils/src/index.ts
@@ -21,5 +21,12 @@ export { default as isFunction } from "./is-function";
 export { default as isNull } from "./is-null";
 export { default as isObject } from "./is-object";
 export { default as isUndefined } from "./is-undefined";
+
 export { default as clone } from "./clone";
 export { default as cloneDeep } from "./clone-deep";
+
+export { default as debounce } from "./debounce";
+export { default as throttle } from "./throttle";
+
+export { default as omitBy } from "./omit-by";
+export { default as objectValueToString } from "./object-value-to-string";
--- packages-utils/src/object-value-to-string/index.ts
+++ packages-utils/src/object-value-to-string/index.ts
@@ -1,6 +1,6 @@
 /**
  * obj 中所有 value 转换为 string
  */
-export function objectValueToString<T extends Record<string, unknown>>(query: T): { [K in keyof T]: string } {
+export default function objectValueToString<T extends Record<string, unknown>>(query: T): { [K in keyof T]: string } {
   return Object.fromEntries(Object.entries(query).map(([key, value]) => [key, String(value)])) as { [K in keyof T]: string };
 }
