6e0729952400bf6a8eb0c480b1b4108c3c8f57e7
李勇 <2236472089@qq.com>
2025-04-03 00:30:16 +0800

    feat: @mt-kit/conf

---
+++ packages-conf/src/iconfont-url/const/index.ts
@@ -0,0 +1,4 @@
+/**
+ * iconfont ——> Font class 的在线地址
+ */
+export const ICONFONT_URL: string = "//at.alicdn.com/t/c/font_4441135_waoyki28kcb.css";
---
+++ packages-conf/src/iconfont-url/enum/index.ts
@@ -0,0 +1,17 @@
+export enum EIconType {
+
+    /**
+     * 箭头
+     */
+    ARROW = "micro-icon-jiantou",
+
+    /**
+     * loading
+     */
+    LOADING = "micro-icon-loading",
+
+    /**
+     * api
+     */
+    API = "micro-icon-API"
+}
