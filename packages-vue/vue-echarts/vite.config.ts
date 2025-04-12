76d7d75d78d97c0f7556d3a341a229e3d63259a0
李勇 <2236472089@qq.com>
2025-04-13 02:30:07 +0800

    feat: 添加 Vue 组件、Echarts 和配置包的初始示例及 Storybook 支持

---
+++ packages-vue/vue-echarts/vite.config.ts
@@ -0,0 +1,13 @@
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+import {
+  defineConfig
+} from "vite";
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx()
+  ]
+});
