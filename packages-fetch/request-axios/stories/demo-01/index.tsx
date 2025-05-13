9b47dc04b76df341178ec134f89623fd22f799bf
李勇 <2236472089@qq.com>
2025-05-14 00:20:15 +0800

    feat(request-axios): 添加请求客户端并更新示例页面
    添加了新的请求客户端实现，包括请求拦截器、响应拦截器和错误处理逻辑。同时更新了示例页面以展示如何使用该客户端进行 API 请求。

--- packages-fetch/request-axios/stories/demo-01/index.tsx
+++ packages-fetch/request-axios/stories/demo-01/index.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-const Index = () => <>demo01</>;
+import {
+  requestClient
+} from "../request";
 
-export default Index;
+requestClient.get("/api/list").then(res => {
+  // eslint-disable-next-line no-console
+  console.log(res);
+});
+
+export default function Index(): React.Element {
+  return (
+    <div>
+      <h1>Demo01</h1>
+      <p>这是一个演示页面</p>
+    </div>
+  );
+}
