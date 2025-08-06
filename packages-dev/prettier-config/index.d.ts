47fe2d13b836de9181d452a82598a224186076d2
李勇 <2236472089@qq.com>
2025-08-06 23:21:29 +0800

    feat: 添加 TypeScript 类型声明文件支持
    - 为 eslint-config 和 prettier-config 添加 index.d.ts 文件，声明模块以支持 TypeScript 类型推断。
    - 更新 package.json 文件，包含类型声明文件路径，确保 TypeScript 能正确识别模块类型。

---
+++ packages-dev/prettier-config/index.d.ts
@@ -0,0 +1,9 @@
+declare module "@mt-kit/prettier-config" {
+
+  // 声明模块存在，让TypeScript自动推断类型
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any;
+  export default content;
+  // eslint-disable-next-line no-restricted-syntax
+  export = content;
+}
