4a1fa3ef764e96957b83c634cd5aacd25ca0f7e5
李勇 <2236472089@qq.com>
2025-09-09 23:55:20 +0800

    feat(demo-file-upload): 新增上传文件测试

--- packages-fetch/request-axios/stories/demo-file-upload/index.tsx
+++ packages-fetch/request-axios/stories/demo-file-upload/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import {
+  requestClient
+} from "../request";
+
 export default function Index(): React.ReactNode {
+  const handleUploadClick = () => {
+    requestClient.upload("/api/file-upload", {
+      file: new File([], "test.txt")
+    });
+  };
+
   return (
     <div>
       <h1>DemoFileUpload</h1>
+
+      <button onClick={handleUploadClick}>上传</button>
     </div>
   );
 }
