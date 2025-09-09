88a6bcb9d89dad4e12aa3d7b26893ff2fe167b0a
李勇 <2236472089@qq.com>
2025-09-09 23:16:03 +0800

    refactor(uploader): 更新 upload 方法

--- packages-fetch/request-axios/src/request-client/modules/uploader.ts
+++ packages-fetch/request-axios/src/request-client/modules/uploader.ts
@@ -4,6 +4,23 @@ import {
 } from "../../types";
 import RequestClient from "../request-client";
 
+interface IDownloadRequestConfig extends RequestClientConfig {
+
+  /**
+   * 上传方法
+   * put上传
+   *
+   * 文件内容通过 Socket 链接直接传输，无额外编码/解码开销，适合大文件上传
+   * 华为云实测：上传 5GB 以下文件时，PUT 比 POST 效率高约 20%
+   *
+   *
+   * post 上传
+   *
+   * 需将文件拆分为多部分并添加边界标识，服务器需解析表单数据，消耗更多CPU和内存
+   */
+  method?: "put" | "post";
+};
+
 class FileUploader {
   private client: RequestClient;
 
@@ -12,41 +29,7 @@ class FileUploader {
   }
 
   /**
-   * put 上传文件
-   *
-   * 文件内容通过Socket链接直接传输，无额外编码/解码开销，适合大文件上传。
-   * 华为云实测：上传5GB以下文件时，PUT比POST效率高约20%。
-   *
-   * @param url 地址
-   * @param data Blob | File
-   * @param config 可选配置对象，继承自Axios请求配置
-   *
-   * onUploadProgress 上传进度回调
-   *
-   * @returns 返回一个Promise，解析值为服务器返回的数据，类型为泛型T
-   */
-  public async upload<T = any>(
-      url: string,
-      data: Blob | File,
-      config?: RequestClientConfig
-  ): Promise<T> {
-
-    const finalConfig: RequestClientConfig = {
-      ...config,
-      headers: {
-        "x-amz-acl": "public-read", // 设置文件的访问权限为公共读
-        "Content-Type": data.type, // 设置文件的MIME类型
-        ...config?.headers
-      }
-    };
-
-    return this.client.put<T>(url, data, finalConfig);
-  }
-
-  /**
-   * post 上传文件
-   *
-   * 需将文件拆分为多部分并添加边界标识，服务器需解析表单数据，消耗更多CPU和内存
+   * 上传文件
    *
    * @param url 地址
    * @param data 数据
@@ -56,11 +39,30 @@ class FileUploader {
    *
    * @returns 返回一个Promise，解析值为服务器返回的数据，类型为泛型T
    */
-  public async postUpload<T = any>(
+  public async upload<T = any>(
       url: string,
       data: Record<string, any> & { file: Blob | File },
-      config?: RequestClientConfig
+      config?: IDownloadRequestConfig
   ): Promise<T> {
+
+    const {
+      method = "post",
+      ...rest
+    } = config || {};
+
+    if(method === "put") {
+      const finalConfig: RequestClientConfig = {
+        ...rest,
+        headers: {
+          "x-amz-acl": "public-read", // 设置文件的访问权限为公共读
+          "Content-Type": data.type, // 设置文件的MIME类型
+          ...rest?.headers
+        }
+      };
+
+      return this.client.put<T>(url, data, finalConfig);
+    }
+
     const formData = new FormData();
 
     Object.entries(data).forEach(([
