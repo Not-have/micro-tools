a379bd86f9c099829c2015f6c7a38392ec25041f
李勇 <2236472089@qq.com>
2025-09-21 16:26:26 +0800

    fix: 确保 props.onClose 安全访问
    - 更新 usePropsOnClose hook，使用可选链操作符确保安全访问 props.onClose，避免潜在的未定义错误

--- packages-react/react-ant-design-extra/src/rc-dialog/model/hooks/use-props-on-close.ts
+++ packages-react/react-ant-design-extra/src/rc-dialog/model/hooks/use-props-on-close.ts
@@ -6,5 +6,5 @@ import useModelProps from "./_use-model-props";
 export default function usePropsOnClose<T extends object = Record<string, unknown>>(): ((result?: T | Error, rejected?: boolean) => void) | undefined {
   const props = useModelProps();
 
-  return props.onClose;
+  return props?.onClose;
 }
