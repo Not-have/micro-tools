a73eeb8ce4dbe9506e3ae8622e26e476fac96f33
李勇 <2236472089@qq.com>
2025-09-27 00:51:37 +0800

    feat: 添加位置信息获取功能
    - 在 index.ts 中导出新的 location 函数。
    - 新增 location.ts 文件，实现获取用户地理位置信息的功能，支持浏览器的 geolocation API。
    - 更新示例 HTML 文件，展示位置信息的获取和显示。

--- packages-utils/src/device/index.ts
+++ packages-utils/src/device/index.ts
@@ -3,3 +3,4 @@ export { default as browser } from "./browser";
 export { default as language } from "./language";
 export { default as onLine } from "./onLine";
 export { default as screen } from "./screen";
+export { default as location } from "./location";
---
+++ packages-utils/src/device/location.ts
@@ -0,0 +1,51 @@
+interface ILocation {
+
+  /**
+   * 纬度
+   */
+  latitude: number;
+
+  /**
+   * 经度
+   */
+  longitude: number;
+}
+
+/**
+ * 📍 位置信息 (Geolocation)
+ *
+ * @returns 返回一个 Promise 对象，resolve 后的值是 ILocation
+ *
+ * 如果浏览器不支持 geolocation，则返回 { latitude: 0, longitude: 0 }
+ */
+export default function location(): Promise<ILocation> {
+  const isSupport = "geolocation" in navigator;
+
+  if(!isSupport) {
+    return Promise.resolve({
+      latitude: 0,
+      longitude: 0
+    });
+  }
+
+  return new Promise(resolve => {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+          const {
+            coords
+          } = position;
+
+          resolve({
+            latitude: Number(coords.latitude.toFixed(6)),
+            longitude: Number(coords.longitude.toFixed(6))
+          });
+        },
+        () => {
+          resolve({
+            latitude: 0,
+            longitude: 0
+          });
+        }
+    );
+  });
+}
