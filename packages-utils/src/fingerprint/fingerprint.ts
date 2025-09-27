7214cd23e03d886cbdd5d4780227878929121eab
李勇 <2236472089@qq.com>
2025-09-27 16:47:10 +0800

    feat: 新增指纹识别功能
    - 新增 fingerprint 函数，生成基于设备信息、音频、Canvas、WebGL 和字体的唯一指纹。
    - 更新 index.ts 文件，导出新功能以供使用。
    - 在示例 HTML 文件中添加指纹识别的调用示例，展示其使用效果。

---
+++ packages-utils/src/fingerprint/fingerprint.ts
@@ -0,0 +1,47 @@
+import {
+  deviceAll
+} from "../device";
+import flattenAndSort from "../flatten-and-sort";
+import sha256Base64 from "../sha256-base64";
+import fingerprintAudio from "./fingerprint-audio";
+import fingerprintCanvas from "./fingerprint-canvas";
+import fingerprintFonts from "./fingerprint-fonts";
+import fingerprintWebgl from "./fingerprint-webgl";
+
+/**
+ * 🫆
+ *
+ * 指纹是根据浏览器环境生成的唯一标识符，用于识别浏览器环境
+ *
+ * @returns string 指纹
+ */
+export default async function fingerprint(): Promise<string> {
+  const device = await deviceAll({
+    location: false,
+    publicIp: false
+  });
+
+  const canvas = fingerprintCanvas();
+
+  const audio = fingerprintAudio();
+
+  const webgl = fingerprintWebgl();
+
+  const fonts = fingerprintFonts();
+
+  const fp = {
+    canvas,
+    audio,
+    webgl,
+    fonts,
+    ...device
+  };
+
+  const flat = flattenAndSort(fp);
+
+  const json = JSON.stringify(flat);
+
+  const hash = await sha256Base64(json);
+
+  return hash;
+}
--- packages-utils/src/fingerprint/index.ts
+++ packages-utils/src/fingerprint/index.ts
@@ -2,3 +2,5 @@ export { default as fingerprintFonts } from "./fingerprint-fonts";
 export { default as fingerprintCanvas } from "./fingerprint-canvas";
 export { default as fingerprintWebgl } from "./fingerprint-webgl";
 export { default as fingerprintAudio } from "./fingerprint-audio";
+
+export { default as fingerprint } from "./fingerprint";
