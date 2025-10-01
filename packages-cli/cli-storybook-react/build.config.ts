60afcb6aee3d33e34923dc4a2af605157e6eda23
李勇 <2236472089@qq.com>
2025-10-01 23:36:27 +0800

    feat: 新增 @mt-kit/storybook-react

---
+++ packages-cli/cli-storybook-react/build.config.ts
@@ -0,0 +1,22 @@
+import {
+  defineBuildConfig
+} from "unbuild";
+
+export default defineBuildConfig({
+  entries: [
+    "src/index.ts"
+  ],
+  clean: true,
+  declaration: true,  // 需保持为 true 才能生成声明文件
+  rollup: {
+
+    // @ts-ignore - unbuild 类型定义中缺少 external 属性
+    external: [
+
+      // React 相关
+      "react",
+      "react-dom",
+      "react-router-dom"
+    ]
+  }
+});
