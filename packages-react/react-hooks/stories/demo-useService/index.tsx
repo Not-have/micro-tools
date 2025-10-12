1c77dde1d3826781bd9a6f6cc6a754a27b76b8d7
苟且 <57132948+Not-have@users.noreply.github.com>
2025-10-12 17:29:45 +0800

    Revert "新增 React hooks"

--- packages-react/react-hooks/stories/demo-useService/index.tsx
+++ packages-react/react-hooks/stories/demo-useService/index.tsx
@@ -1,49 +1,22 @@
-import React, {
-  useCallback
-} from "react";
+import React from "react";
 
 import {
   useService
 } from "../../src";
 import {
-  testFetch,
-  IResponse
+  testFetch
 } from "../fetch";
 
 export default function Index(): React.ReactElement {
   const {
     data,
-    loading,
-    run
-  } = useService<[string, string], IResponse>(testFetch, [
-    "123",
-    "v1.0"
-  ], {
-    debounce: 2000
-  });
+    loading
+  } = useService(testFetch);
 
   // eslint-disable-next-line no-console
   console.log(data, loading);
 
-  const handleClick = useCallback(() => {
-    run("456", "v2.0");
-  }, [
-    run
-  ]);
-
   return <div>
     <p>useService</p>
-    <button onClick={handleClick}>Fetch Data</button>
-    <hr />
-    {data?.content}
-    {loading ? "loading" : "not loading"}
-    {data?.title}
-    {data?.author}
-    {data?.url}
-    <hr />
-
-    <code>
-      {JSON.stringify(data, null, 2)}
-    </code>
   </div>;
 }
