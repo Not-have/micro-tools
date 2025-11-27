fff8213067221d63edca02b263417088ac327428
李勇 <2236472089@qq.com>
2025-11-28 00:43:54 +0800

    feat: 滚动条

--- packages-vue/vue-components/src/scrollbar/main/index.vue
+++ packages-vue/vue-components/src/scrollbar/main/index.vue
@@ -1,5 +1,61 @@
+<script lang="ts" setup>
+import {
+  ref
+} from "vue";
+
+const {
+  always = false
+} = defineProps<{
+
+  /**
+   * 是否总是显示滚动条
+   *
+   * @default false
+   */
+  always?: boolean;
+}>();
+
+const scrollbarRef = ref<HTMLDivElement | null>(null);
+
+</script>
 <template>
-  <div>
-    1
+  <div
+    ref="scrollbarRef"
+    class="scrollbar"
+    :class="{
+      'scrollbar-always': always
+    }"
+  >
+    <slot></slot>
   </div>
 </template>
+<style scoped>
+.scrollbar {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+}
+
+.scrollbar-always {
+  overflow: scroll;
+
+  /* 用于解决滚动条抖动问题 */
+  scrollbar-gutter: stable;
+}
+
+.scrollbar-always::-webkit-scrollbar {
+  /* 滚动条宽度 */
+  width: 7px;
+  height: 7px;
+}
+
+.scrollbar-always::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+  box-shadow: 0 0 1px rgb(255 255 255 / 50%);
+  background-color: rgb(0 0 0 / 50%);
+}
+
+.scrollbar-always::-webkit-scrollbar-track {
+  background-color: transparent;
+}
+</style>
--- packages-vue/vue-components/stories/demo-scrollbar/index.vue
+++ packages-vue/vue-components/stories/demo-scrollbar/index.vue
@@ -1,3 +1,42 @@
+<script lang="ts" setup>
+import {
+  Scrollbar
+} from "../../src";
+</script>
 <template>
-  demo
+  <div class="demo-scrollbar">
+    <Scrollbar>
+      <div class="demo-scrollbar-item">
+        1
+      </div>
+      <div class="demo-scrollbar-item">
+        2
+      </div>
+      <div class="demo-scrollbar-item">
+        3
+      </div>
+      <div class="demo-scrollbar-item">
+        4
+      </div>
+      <div class="demo-scrollbar-item">
+        5
+      </div>
+      <div class="demo-scrollbar-item">
+        6
+      </div>
+    </Scrollbar>
+  </div>
 </template>
+<style scoped>
+.demo-scrollbar {
+  width: 100%;
+  height: 60vh;
+  overflow: hidden;
+}
+
+.demo-scrollbar-item {
+  background-color: aqua;
+  width: 200px;
+  height: 400px;
+}
+</style>
