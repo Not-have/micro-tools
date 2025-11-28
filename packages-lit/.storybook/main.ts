0f8562d175c1affeac30974c99d68f3c64e5518f
李勇 <2236472089@qq.com>
2025-11-29 01:45:55 +0800

    feat: 添加按钮和表格组件及其样式，并配置 Storybook

---
+++ packages-lit/.storybook/main.ts
@@ -0,0 +1,21 @@
+import type {
+  StorybookConfig
+} from "@storybook/web-components-vite";
+
+const config: StorybookConfig = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/experimental-addon-test"
+  ],
+  "framework": {
+    "name": "@storybook/web-components-vite",
+    "options": {}
+  }
+};
+
+export default config;
